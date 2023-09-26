466c78f74249895ff32423919e73d225cf4733f9
Lavanya <lavanyaofficial26@gmail.com>
2023-09-25 20:04:45 -0400

    Initial commit

---
+++ src/main/java/com/jrcodecrew/codeschool/CodeschoolApplication.java
@@ -0,0 +1,13 @@
+package com.jrcodecrew.codeschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeschoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeschoolApplication.class, args);
+	}
+
+}
