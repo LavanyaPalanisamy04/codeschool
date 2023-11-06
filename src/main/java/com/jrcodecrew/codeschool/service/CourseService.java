2e9e37f4b7f3b7e40eb0610d4f9fab0ba1aeb319
LavanyaPalanisamy <124698532+LavanyaPalanisamy04@users.noreply.github.com>
2023-11-06 12:22:48 -0500

    Course apis (#5)
    * Added Model for Course Table
    
    * Added new files for course apis
    
    * added apis related to course

---
+++ src/main/java/com/jrcodecrew/codeschool/service/CourseService.java
@@ -0,0 +1,12 @@
+package com.jrcodecrew.codeschool.service;
+
+import com.jrcodecrew.codeschool.dto.CourseDto;
+import com.jrcodecrew.codeschool.model.Course;
+
+import java.util.List;
+
+public interface CourseService {
+  Course addCourse(CourseDto courseDto);
+  Course getCourseById(String courseId);
+  List<Course> getCoursesByAgeGroup(String ageGroup);
+}
